3a9e49fc743efedc2d16543366c36963161a9e9d
vknhu <khanhnhuvu@gmail.com>
2024-10-23 14:08:10 +1030

    Implemented the first stage in the Login
    - Added the useState for username and password
    - Implemented the fetch for the Login

---
+++ Assig2/a2reactapp/src/components/Login.js
@@ -0,0 +1,26 @@
+﻿import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
+function Login() => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginState, setLoginState] = useState(false);
+    const [triggerFetch, setTriggerFetch] = useState(false);
+
+    React.useEffect(() => {
+        const passwordHash = CryptoJS.SHA256(password).toString();
+
+        fetch(`http://localhost:5137/api/Login?userName=${username}&passwordHash=${passwordHash}`)
+            .then(response => response.json)
+            .then(data => setState(data))
+            .catch(err => { console.log(err); })
+    });
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setTriggerFetch(true);
+    }
+
+    return ()
+}
+
+export default Login
