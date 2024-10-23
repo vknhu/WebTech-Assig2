71496d2b50dd33f9f50769d97203c7a9332c2c1c
vknhu <khanhnhuvu@gmail.com>
2024-10-23 21:29:32 +1030

    Implemented the fetch request
    Fixed the URL in fetch request, added the userName and passwordHash as parameters to send to EndpointController

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -1,30 +1,27 @@
 ﻿import React, { useState } from 'react';
 import CryptoJS from 'crypto-js';
-function Login() => {
+function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [loginState, setLoginState] = useState(false);
+    const [loginStatus, setLoginStatus] = useState(false);
 
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        const passwordHash = CryptoJS.SHA256(password).toString();
-        try {
-            const response = await fetch(`http://localhost:5137/api/Login?userName=${username}&passwordHash=${passwordHash}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ userName: username, passwordHash }),
+    function handleLogin() {
+        const passwordHashed = CryptoJS.SHA256(password).toString();
+
+        fetch(`http://localhost:5137/api/Login?userName=${username}&passwordHash=${passwordHashed}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',}
+        }).then(response => response.json())
+            .then(data => {
+                setLoginStatus(data? 'Login Successful' : 'Login Failed');
+            })
+            .catch(err => {
+                console.log(err);
+                setLoginStatus('An error occurred while logging in');
             });
-        } catch (error) {
-            console.error('Error during login:', error);
-            setLoginStatus('An error occurred during the login process.');
-        }
 
+        return ();
     }
 
-    return ();
-}
-
-export default Login
+    export default Login
