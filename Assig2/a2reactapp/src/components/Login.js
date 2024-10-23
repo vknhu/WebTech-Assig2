72482dfe3dc4b0f7484a105af8ec7cdd2e070ba7
vknhu <khanhnhuvu@gmail.com>
2024-10-23 21:40:04 +1030

    Implemented the loginForm in Login.js

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -11,17 +11,51 @@ function Login() {
 
         fetch(`http://localhost:5137/api/Login?userName=${username}&passwordHash=${passwordHashed}`, {
             method: 'POST',
-            headers: {'Content-Type': 'application/json',}
+            headers: { 'Content-Type': 'application/json', }
         }).then(response => response.json())
             .then(data => {
-                setLoginStatus(data? 'Login Successful' : 'Login Failed');
+                setLoginStatus(data ? 'Login Successful' : 'Login Failed');
             })
             .catch(err => {
                 console.log(err);
                 setLoginStatus('An error occurred while logging in');
             });
 
-        return ();
+        return (
+            <div className="login">
+                <h3>Sign in</h3>
+                <form className="loginForm">
+                    <div className="inputGroup">
+                        <label htmlFor="userName">Username:</label>
+                        <input
+                            type="text"
+                            id="userName"
+                            name="userName"
+                            autoComplete="off"
+                            placeholder="Enter your username"
+                        />
+                        <label htmlFor="Password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            autoComplete="off"
+                            placeholder="Enter your Password"
+                        />
+                        <button type="submit" class="btn btn-primary">
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <div className="login">
+                    <p>Don't have Account? </p>
+                    <Link to="/" type="submit" class="btn btn-success">
+                        Register
+                    </Link>
+                </div>
+            </div>
+        );
     }
+}
 
-    export default Login
+export default Login;
