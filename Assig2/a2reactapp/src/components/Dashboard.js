e6ae10b949e5a739d6987d22fd9e351df4120655
vknhu <khanhnhuvu@gmail.com>
2024-10-25 12:00:25 +1030

    Added the button to direct to Report page

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -44,7 +44,7 @@ function Dashboard() {
                         </div>
                     </div>
                 </form>
-                <Link to="/" className="btn btn-success">Show Report</Link>
+                <Link to="/Report" className="btn btn-success">Show Report</Link>
             </div>
         </div>
     )
