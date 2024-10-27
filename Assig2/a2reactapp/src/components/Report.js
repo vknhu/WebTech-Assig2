8bba04e258bc89f90ca92cc03c8d0133edc22eb2
vknhu <khanhnhuvu@gmail.com>
2024-10-27 15:55:48 +1030

    Added the link to logout from the Report page and direct to the Login page

--- Assig2/a2reactapp/src/components/Report.js
+++ Assig2/a2reactapp/src/components/Report.js
@@ -3,7 +3,16 @@ import { Link } from "react-router-dom";
 
 function Report() {
     return (
-        <Link to="/Dashboard" class="btn btn-success">Back to Dashboard</Link>
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center text-light bg-gradient p-2 my-2 border rounded">
+                <h2>MPDC Report</h2>
+                <p class="mb-0">
+                    <Link to="/Login" class="text-light">Log out</Link>
+                </p>
+            </div>
+            <Link to="/Dashboard" class="btn btn-success">Back to Dashboard</Link>
+        </div>
+
     )
 }
 
