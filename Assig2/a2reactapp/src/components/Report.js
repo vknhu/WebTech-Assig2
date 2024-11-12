ceefd90ba352f8a02d7c54b2ec33e5fbdc0c91f9
vknhu <khanhnhuvu@gmail.com>
2024-11-12 23:06:02 +1030

    Added the template for the report page

--- Assig2/a2reactapp/src/components/Report.js
+++ Assig2/a2reactapp/src/components/Report.js
@@ -26,6 +26,28 @@ function Report() {
                     <Link to="/Login" onClick={handleLogout} class="text-light">Log out</Link>
                 </p>
             </div>
+            <h3>Introduction</h3>
+            <p>This report outlines two priority locations for the installation of Mobile Phone Detection Cameras (MPDCs) based on an analysis
+                of expiation data obtained from the SAPOL database. The locations were selected using the MPDC Dashboard in the web application,
+                which features a variety of search and filtering mechanisms for optimal site identification. Below, I explain the process used
+                to determine these locations, along with relevant data visualizations to support the recommendations.</p>
+            <h3>Selected Location</h3>
+            <ul>
+                <ol>LocationId:</ol>
+                <ol>LocationId:</ol>
+            </ul>
+            <p>filters and search options used in the dashboard to identify these sites</p>
+            <h3>Visual Evidence and Data Representation</h3>
+            <div className="row">
+                <img src="" placeholder="Google Maps Screenshot of location 1" />
+                <img src="" placeholder="Google Maps Screenshot of location 2" />
+            </div>
+            <h3>Data visualization</h3>
+            <div className="row">
+                <p>Graph 1</p>
+                <p>Graph 2</p>
+            </div>
+            <h3>Explanation for location selection</h3>
             <Link to="/Dashboard" class="btn btn-success">Back to Dashboard</Link>
         </div>
 
